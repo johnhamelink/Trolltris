7f73a6ff171a63f888f5ee988b06cc95f37c449c
John Hamelink <john@johnhamelink.com>
2011-10-22 22:07:21 +0100

    no message

--- app.js
+++ app.js
@@ -227,7 +227,7 @@ function blockDown( userId ){
 
 function blockAdd( userId ){
     var startpoint = parseInt(Math.floor( Math.random() * width ));
-    console.log(blockmap[blockmap.length][startpoint]);
+    console.log(blockmap[blockmap.length]);
     blockmap[blockmap.length][startpoint] = userId;
 }
 
