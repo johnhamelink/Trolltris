f1b4c422be9a748a3fa1d80f7171e20d1efc7e81
John Hamelink <john@johnhamelink.com>
2011-10-22 12:49:45 +0100

    Basic structure of NowJS stuff

--- app.js
+++ app.js
@@ -5,7 +5,9 @@
 
 var express = require('express');
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer(),
+    nowjs = require('now'),
+    everyone = nowjs.initialize(app);
 
 // Configuration
 
@@ -29,10 +31,75 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+  res.render('index');
 });
+
+function buildWellMap(x, y){
+    var well = [];
+    for( horizontal = 0; horizontal <= x; horizontal++ ){
+        for ( vertical = 0; vertical <= y; vertical++ ){
+            well.push({
+                x: horizontal,
+                y: vertical,
+                occupied: 0
+            });
+        }
+    }
+
+    return well;
+}
+
+// For each user, userID we have coords and current block ID
+var users = [];
+var well = buildWellMap(10,20);
+
+
+console.log(well);
+
+// Broadcast new map to all clients
+everyone.now.updatemap = function(){
+    
+}
+
+// Move one block to the left
+everyone.now.moveleft = function(userID, blockID){
+    
+}
+
+// Move one block to the right
+everyone.now.moveright = function(userID, blockID){
+    
+}
+
+// Rotate one block 90 degrees to the left
+everyone.now.rotateleft = function(userID, blockID){
+    
+}
+
+// Rotate one block 90 degrees to the right
+everyone.now.rotateright = function(userID, blockID){
+    
+}
+
+// Move down a block
+everyone.now.moveDown = function(userID, blockID){
+    
+}
+
+
+
+// Check to see if a line has been filled.
+function checkLineFilled(){
+    
+}
+
+// Check if the bricks have hit the top
+// of the well - game over.
+function checkWellFilled(){
+    
+}
+
+
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
